87a479977221338b3e61f1f1601fe277d72480d8
mickaelgudin <mickaelgudin@gmail.com>
2021-03-05 22:06:14 +0100

    CI job + exposition api de base pour trainStations

--- src/main/java/com/spring/crud/demo/service/impl/TrainStationServiceImpl.java
+++ src/main/java/com/spring/crud/demo/service/impl/TrainStationServiceImpl.java
@@ -1,6 +1,10 @@
 package com.spring.crud.demo.service.impl;
 
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import com.spring.crud.demo.model.TrainStation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,9 @@ public class TrainStationServiceImpl implements TrainStationService {
 
 	@Autowired
 	private TrainStationRepository repository;
+	
+	@PersistenceContext
+	private EntityManager entityManger;
 
 	@Override
 	public List<TrainStation> getAll() {
@@ -39,6 +46,26 @@ public class TrainStationServiceImpl implements TrainStationService {
 				return trainStation;
 		}
 		return null;
+	}
+
+	@Override
+	public TrainStation save(TrainStation trainStation) {
+		return repository.save(trainStation);
+	}
+
+	@Override
+	public TrainStation update(int id, TrainStation trainStation) {
+		if(!repository.existsById(id) ) return null;
+    	
+		trainStation.setId(id);
+        return repository.save(trainStation);
+	}
+
+	@Override
+	public void delete(int id) {
+		if(repository.existsById(id) ) {
+			repository.deleteById(id);
+		}
 	}
 
 }
