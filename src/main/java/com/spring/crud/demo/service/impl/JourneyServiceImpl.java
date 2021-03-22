75fcfdcaae5ce4e603831a375f9de76c47e0c3d6
mickaelgudin <mickaelgudin@gmail.com>
2021-03-22 23:02:49 +0100

    getTendancy : ajout de verification des parametres

--- src/main/java/com/spring/crud/demo/service/impl/JourneyServiceImpl.java
+++ src/main/java/com/spring/crud/demo/service/impl/JourneyServiceImpl.java
@@ -1,10 +1,12 @@
 package com.spring.crud.demo.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.spring.crud.demo.model.Journey;
 import com.spring.crud.demo.model.TrainStation;
 import com.spring.crud.demo.repository.JourneyRepository;
+import com.spring.crud.demo.repository.TrainStationRepository;
 import com.spring.crud.demo.service.JourneyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,10 @@ public class JourneyServiceImpl implements JourneyService {
 
 	@Autowired
 	private JourneyRepository repository;
-
+	@Autowired
+	private TrainStationRepository repositoryStation;
+	
+	
 	@Override
 	public List<Journey> getAll() {
 		return repository.findAll();
@@ -25,14 +30,25 @@ public class JourneyServiceImpl implements JourneyService {
 		return repository.save(journey);
 	}
 
+	private TrainStation checkIfStationExist(int idStation) {
+		Optional<TrainStation> stationOptionnal= repositoryStation.findById(idStation);
+		if(stationOptionnal.isPresent() ) {
+			return stationOptionnal.get();
+		} 
+		
+		return null;
+	}
 	
 	@Override
 	public String getTendancy(int idStationDepart, int idStationArrival) {
-		TrainStation departStation = entityManager.find(TrainStation.class, idStationDepart);
-		TrainStation arrivalStation = entityManager.find(TrainStation.class, idStationArrival);
+		TrainStation departStation = checkIfStationExist(idStationDepart);
+		if(departStation == null) return "departure station doesn't exist";
+		
+		TrainStation arrivalStation = checkIfStationExist(idStationArrival);
+		if(arrivalStation == null)	return "arrival station doesn't exist";
+		
 		
 		List<Journey> filteredJourneys = repository.getJourneysOfDestination(departStation, arrivalStation);
-
 		if(filteredJourneys.isEmpty()) {
 			return "no data yet";
 		}
